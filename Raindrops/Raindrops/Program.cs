e1a64109dedb72483ad03c9ee8f73cfcd6d5790f
jsahota91 <jsahota@spartaglobal.com>
2021-05-27 13:53:01 +0100

    refactoring the code

--- Raindrops/Raindrops/Program.cs
+++ Raindrops/Raindrops/Program.cs
@@ -6,22 +6,22 @@ namespace Raindrops
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(Factors(28));
-            Console.WriteLine(Factors(34));
-            Console.WriteLine(Factors(30));
+            Console.WriteLine(FactorResults(28));
+            Console.WriteLine(FactorResults(30));
+            Console.WriteLine(FactorResults(34));
         }
 
-        public static string Factors(int n)
+        public static string FactorResults(int n)
         {
-            string result = "";
-            if (n % 105 == 0) return result += "PlingPlangPlong";
-            else if (n % 35 == 0) return result += "PlangPlong";
-            else if (n % 21 == 0) return result += "PlingPlong";
-            else if (n % 15 == 0) return result += "PlingPlang";
-            else if (n % 7 == 0) return result += "Plong";
-            else if (n % 5 == 0) return result += "Plang";
-            else if (n % 3 == 0) return result += "Pling";
-            else return result += n.ToString();
+            string resultShort = (n % 105 == 0) ? "PlingPlangPlong" :
+                                 (n % 35 == 0) ? "PlangPlong" :
+                                 (n % 21 == 0) ? "PlingPlong" :
+                                 (n % 15 == 0) ? "PlingPlang" :
+                                 (n % 7 == 0) ? "Plong" :
+                                 (n % 5 == 0) ? "Plang" :
+                                 (n % 3 == 0) ? "Pling" :
+                                 n.ToString();
+            return resultShort;
         }
 
     }
--- Raindrops/RaindropsTest/FactorsShould.cs
+++ Raindrops/RaindropsTest/FactorsShould.cs
@@ -9,56 +9,56 @@ namespace RaindropsTest
         [TestCase(14, "Plong")]
         public void GivenNumberNHasAFactorOf7_ReturnStringPlong(int n, string expected)
         {
-            Assert.That(Program.Factors(n), Is.EqualTo(expected));
+            Assert.That(Program.FactorResults(n), Is.EqualTo(expected));
         }
 
         [TestCase(23, "23")]
         [TestCase(34, "34")]
         public void GivenNumberNHasNoneOfTheFactors_ReturnNumberN(int n, string expected)
         {
-            Assert.That(Program.Factors(n), Is.EqualTo(expected));
+            Assert.That(Program.FactorResults(n), Is.EqualTo(expected));
         }
 
         [TestCase(10, "Plang")]
         [TestCase(25, "Plang")]
         public void GivenNumberNHasFactorOf5_ReturnStringPlang(int n, string expected)
         {
-            Assert.That(Program.Factors(n), Is.EqualTo(expected));
+            Assert.That(Program.FactorResults(n), Is.EqualTo(expected));
         }
 
         [TestCase(12, "Pling")]
         [TestCase(9, "Pling")]
         public void GivenNumberNHasFactorOf3_ReturnStringPling(int n, string expected)
         {
-            Assert.That(Program.Factors(n), Is.EqualTo(expected));
+            Assert.That(Program.FactorResults(n), Is.EqualTo(expected));
         }
 
         [TestCase(15, "PlingPlang")]
         [TestCase(30, "PlingPlang")]
         public void GivenNumberNHasFactorOf3and5_ReturnStringPlingPlang(int n, string expected)
         {
-            Assert.That(Program.Factors(n), Is.EqualTo(expected));
+            Assert.That(Program.FactorResults(n), Is.EqualTo(expected));
         }
 
         [TestCase(21, "PlingPlong")]
         [TestCase(42, "PlingPlong")]
         public void GivenNumberNHasFactorOf3and7_ReturnStringPlingPlong(int n, string expected)
         {
-            Assert.That(Program.Factors(n), Is.EqualTo(expected));
+            Assert.That(Program.FactorResults(n), Is.EqualTo(expected));
         }
 
         [TestCase(35, "PlangPlong")]
         [TestCase(70, "PlangPlong")]
-        public void GivenNumberNHasFactorOf3and7_ReturnStringPlangPlong(int n, string expected)
+        public void GivenNumberNHasFactorOf5and7_ReturnStringPlangPlong(int n, string expected)
         {
-            Assert.That(Program.Factors(n), Is.EqualTo(expected));
+            Assert.That(Program.FactorResults(n), Is.EqualTo(expected));
         }
 
         [TestCase(105, "PlingPlangPlong")]
         [TestCase(210, "PlingPlangPlong")]
         public void GivenNumberNHasFactorOf3and5and7_ReturnStringPlingPlangPlong(int n, string expected)
         {
-            Assert.That(Program.Factors(n), Is.EqualTo(expected));
+            Assert.That(Program.FactorResults(n), Is.EqualTo(expected));
         }
     }
 }
